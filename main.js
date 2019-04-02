f8b353960c6f98343933d2cc2f68013dab76ad7a
Christophe Schevers <christophe.schevers@gmail.com>
2019-04-02 09:23:55 +0200

    New commit

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 document.getElementById("button").addEventListener("click", loadPokemon);
-document.getElementById("button").addEventListener("click", loadPrevPokemon);
 
 const page = document.getElementById("output");
 
@@ -31,25 +30,41 @@ function loadPokemon() {
             </li>`;
             
             page.innerHTML = output;
+            loadPrevPokemon(url);
         }
     }
     
     xhr.send();
 }
 
-function loadPrevPokemon() {
-    let pokemon = document.getElementById("search").value;
-
+function loadPrevPokemon(x) {
     const xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "https://pokeapi.co/api/v2/pokemon-species/" + pokemon, true);
+    xhr.open("GET", x, true);
 
     xhr.onload = function() {
         if (this.status == 200) {
             let pokEvol = JSON.parse(this.response);
-            console.log(pokEvol);
+            let urlPic = pokEvol.evolves_from_species.url;
+            console.log(urlPic);
 
             page.innerHTML += pokEvol.evolves_from_species.name;
+            loadPrevPokePicture(urlPic);
+        }
+    }
+
+    xhr.send();
+}
+
+function loadPrevPokePicture(y) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", y, true);
+
+    xhr.onload = function() {
+        if (this.status == 200) {
+            let pokePic = JSON.parse(this.response);
+            console.log(pokePic);
         }
     }
 
